7e55e7161d164c820b19ed061347219a9e098dab
Abel_PC <569879601@qq.com>
2022-03-07 21:56:59 +0800

    学习红黑树；

--- test_rbtree/test_rbtree.c
+++ test_rbtree/test_rbtree.c
@@ -50,6 +50,10 @@ void left_rotate(struct tree* T, struct node* x) {
 
 }
 
+/**
+ * 右旋转
+ * 
+ */
 void right_rotate(struct tree* T, struct node* x) {
 	struct node* y;
 
@@ -68,8 +72,53 @@ void right_rotate(struct tree* T, struct node* x) {
 	x->p = y;
 }
 
+/**
+* 重新着色并旋转
+*/
 void rb_insert_fixup(struct tree* T, struct node* z) {
-
+	struct node* y;
+	while (z->p->color == RED)
+	{
+		if (z->p == z->p->p->left)
+		{
+			y = z->p->p->right;
+			if (y->color == RED)
+			{
+				z->p->color = BLACK;
+				y->color = BLACK;
+				z->p->p->color = RED;
+				z = z->p->p;
+			}
+			else if (z == z->p->right)
+			{
+				z = z->p;
+				left_rotate(T, z);
+			}
+			z->p->color = BLACK;
+			z->p->p->color = RED;
+			right_rotate(T, z->p->p);
+		}
+		else
+		{
+			y = z->p->p->left;
+			if (y->color == RED)
+			{
+				z->p->color = BLACK;
+				y->color = BLACK;
+				z->p->p->color = RED;
+				z = z->p->p;
+			}
+			else if (z == z->p->left)
+			{
+				z = z->p;
+				right_rotate(T, z);
+			}
+			z->p->color = BLACK;
+			z->p->p->color = RED;
+			left_rotate(T, z->p->p);
+		}
+	}
+	T->root->color = BLACK;
 }
 
 void rb_insert(struct tree* T,struct node* z ) {
@@ -99,6 +148,23 @@ void rb_insert(struct tree* T,struct node* z ) {
 	z->color = RED;
 
 	rb_insert_fixup(T, z);
+}
+
+void rb_transplant(struct tree* T, struct node* u, struct node* v)
+{
+	if (u->p == 0)
+	{
+		T->root = v;
+	}
+	else if (u == u->p->left)
+	{
+		u->p->left = v;
+	}
+	else
+	{
+		u->p->right = v;
+	}
+	v->p = u->p;
 }
 
 int main(int argn, char* argc[])
